6d939a84ec10cfcf3533b2fc336e3c2fd78ae764
Richard Davis <rich@davisfamily.eu>
2019-02-07 18:21:29 +0000

    Tidied FMD Parser (still not great)

--- app/src/main/java/com/example/advancedscanning/FMDBarCode.java
+++ app/src/main/java/com/example/advancedscanning/FMDBarCode.java
@@ -62,41 +62,35 @@ public class FMDBarCode {
             bc.setGtin(gs1.substring(2, 16));
             System.out.println(bc.getGtin());
             return processGS1Data(bc, gs1.substring(16));
-
         }else if (gs1.substring(0,2).equals("17")){
             System.out.println("Expiry detected");
             bc.setExpiry(gs1.substring(2, 8));
             System.out.println(bc.getExpiry());
             return processGS1Data(bc, gs1.substring(8));
+        }else {
+            return processVariableLengthField(bc, gs1);
+        }
+    }
 
-        }else if (gs1.substring(0,2).equals("10")){
-            System.out.println("Batch detected");
-            int igs = getIndexOfGSChar(gs1, 20);
-            int endIndex = gs1.length();
-            endIndex = endIndex < 22 ? endIndex : 22;
-            if (igs > -1) {
-                endIndex = igs;
-            }
-
-            bc.setBatch(gs1.substring(2, endIndex));
-            System.out.println(bc.getBatch());
-            endIndex = igs + 1 > endIndex ? igs + 1 : endIndex;
-            return processGS1Data(bc, gs1.substring(endIndex));
-        }else if (gs1.substring(0,2).equals("21")){
+    private static FMDBarCode processVariableLengthField (FMDBarCode bc, String gs1){
+        int igs = getIndexOfGSChar(gs1, 20);
+        int endIndex = gs1.length();
+        endIndex = endIndex < 22 ? endIndex : 22;
+        if (igs > -1) {
+            endIndex = igs;
+        }
+        String code = gs1.substring(0,2);
+        if (code.equals("21")) {
             System.out.println("Serial detected");
-            int igs = getIndexOfGSChar(gs1, 20);
-            int endIndex = gs1.length();
-            endIndex = endIndex < 22 ? endIndex : 22;
-            if (igs > -1) {
-                endIndex = igs;
-            }
             bc.setSerial(gs1.substring(2, endIndex));
             System.out.println(bc.getSerial());
-            endIndex = igs + 1 > endIndex ? igs + 1 : endIndex;
-            return processGS1Data(bc, gs1.substring(endIndex));
+        }else{
+            System.out.println("Batch detected");
+            bc.setBatch(gs1.substring(2, endIndex));
+            System.out.println(bc.getBatch());
         }
-        return bc;
-
+        endIndex = igs + 1 > endIndex ? igs + 1 : endIndex;
+        return processGS1Data(bc, gs1.substring(endIndex));
     }
 
     private static int getIndexOfGSChar(String s, int testLen){
